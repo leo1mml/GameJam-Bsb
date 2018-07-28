2d45ef61ca3aa73245def7c7e70a3be5bb8e3e3d
Unknown <winstoncire@gmail.com>
2018-07-28 04:25:05 -0300

    Creating Player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    private float horizontalDirection;
+    private float verticalDirection;
+    public float velocity;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalDirection = Input.GetAxis("Horizontal");
+        verticalDirection = Input.GetAxis("Vertical");
+        rb.AddForce(new Vector2(horizontalDirection * velocity, verticalDirection * velocity));
+
+
+        if (Mathf.Approximately(horizontalDirection, 0.0f))
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        if (Mathf.Approximately(verticalDirection, 0.0f))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+        }
+
+    }
+}
