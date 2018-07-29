4011d46c90a9bf15acc6ba466bbae89ee75b6928
Unknown <eduardo.torresm@hotmail.com>
2018-07-29 17:15:28 -0300

    credit scene added

---
+++ Assets/TextCrontroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextCrontroller : MonoBehaviour {
+
+	// Use this for initialization
+	public bool canScroll;
+	private RectTransform transform ;
+	public float scrollSpeed;
+	void Start () {
+		transform = GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private void FixedUpdate() {
+		if(canScroll){
+			transform.position += Vector3.up*scrollSpeed;
+		}
+	}
+}
