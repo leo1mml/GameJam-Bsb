af78bf5495ad9216142543f7a24ee454c837d8f6
Unknown <eduardo.torresm@hotmail.com>
2018-07-29 17:34:17 -0300

    scroll working

--- Assets/TextCrontroller.cs
+++ Assets/TextCrontroller.cs
@@ -7,9 +7,14 @@ public class TextCrontroller : MonoBehaviour {
 	// Use this for initialization
 	public bool canScroll;
 	private RectTransform transform ;
+	public RectTransform canvasRect;
+	private RectTransform childPoint;
+	public RectTransform body;
 	public float scrollSpeed;
 	void Start () {
 		transform = GetComponent<RectTransform>();
+		//canvasRect = GetComponentInParent<RectTransform>();
+		childPoint = body.GetComponentInChildren<RectTransform>();
 	}
 	
 	// Update is called once per frame
@@ -19,6 +24,11 @@ public class TextCrontroller : MonoBehaviour {
 	private void FixedUpdate() {
 		if(canScroll){
 			transform.position += Vector3.up*scrollSpeed;
+			if(childPoint.position.y > canvasRect.position.y){
+				canScroll = false;
+			}
+			print(childPoint.position+ "??" + canvasRect.position) ;
+
 		}
 	}
 }
